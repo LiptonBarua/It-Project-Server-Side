a55d182d2df1f9bb709e6ac27b66a898ac995504
Lipton Barua <liptonbarua274@gmail.com>
2023-05-25 13:26:37 +0600

    project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app= express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('It Project Run in Browser')
 })
 
@@ -20,7 +20,7 @@ app.get('/', (req, res)=>{
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.l1pkovt.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -31,46 +31,95 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
-    const usersCollection=client.db('itSolutionsData').collection('users')
+
+    const usersCollection = client.db('itSolutionsData').collection('users')
+    const informationCollection = client.db('itSolutionsData').collection('personalInformation')
+    const licenseMemberCollection = client.db('itSolutionsData').collection('licenseMember')
 
 
     // ................User Collection.................
 
-    app.post('/users', async(req, res)=>{
-      const user=req.body;
-      const result =await usersCollection.insertOne(user);
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
       res.send(result)
     })
 
 
-    app.get('/users', async(req, res)=>{
-      const query={};
-      const result= await usersCollection.find(query).toArray();
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
       res.send(result)
     })
-    app.get('/profile', async(req, res)=>{
-      let query={};
-      if(req.query.email){
-        query= {
-          email:req.query.email
+
+    app.put('/profile', async (req, res) => {
+      const userProfile = req.query.email;
+      const file = req.body;
+      const { firstName, lastName, email, phone, image, location } = file;
+      const filter = { email: userProfile };
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          firstName, lastName, email, phone, image, location
         }
       }
-      const result= await usersCollection.find(query).toArray();
+
+      const result = await usersCollection.updateOne(filter, updatedDoc, option)
+      res.send(result)
+    })
+
+
+    app.get('/profile', async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email
+        }
+      }
+      const result = await usersCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    // ...................Personal Information.............
+
+    app.post('/information', async (req, res) => {
+      const query = req.body;
+      const result = await informationCollection.insertOne(query)
+      res.send(result)
+    })
+
+
+    app.get('/information', async (req, res) => {
+      const query = {};
+      const result = await informationCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    //  ....................Personal License Members.....................
+
+    app.post('/licensemember', async (req, res) => {
+      const query = req.body;
+      const result = await licenseMemberCollection.insertOne(query)
+      res.send(result)
+    })
+
+    app.get('/licensemember', async (req, res) => {
+      const query = {};
+      const result = await licenseMemberCollection.find(query).toArray();
       res.send(result)
     })
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
   }
 }
-run().catch(error=>console.error(error));
+run().catch(error => console.error(error));
 
 
 
 
-app.listen(port, (req, res)=>{
-    console.log('it project run in cmd')
+app.listen(port, (req, res) => {
+  console.log('it project run in cmd')
 })
